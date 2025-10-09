9c941a36caf67a3f6e080b8e4307e6dbd8e73ef4
Vetal <heavyve@gmail.com>
2025-10-09 20:03:52 +0300

    add solution javarush linux level05 task 04

---
+++ src/ru/javarush/system/linux/level05/task04/solution.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Проверка, установлен ли ufw, и установка, если отсутствует
+sudo apt update
+sudo apt install -y ufw
+
+# Включение брандмауэра
+sudo ufw enable
+
+# Разрешение трафика для SSH (порт 22)
+sudo ufw allow 22
+
+# Разрешение трафика для HTTP (порт 80)
+sudo ufw allow 80
+
+# Запрет всех остальных подключений
+sudo ufw default deny incoming
+
+# Вывод текущих правил брандмауэра
+sudo ufw status verbose
