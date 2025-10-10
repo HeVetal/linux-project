368cd3a4d554e712d5246cb588d75754ed2f77be
Vetal <heavyve@gmail.com>
2025-10-10 10:52:00 +0300

    add solution javarush linux level05 task 13

---
+++ src/ru/javarush/system/linux/level05/task13/solution.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Создаем файл testfile.txt, если он не существует
+touch testfile.txt
+
+# Устанавливаем ACL права по умолчанию (необязательно, если файл создан только что)
+?
+
+# Выводим текущие ACL права файла testfile.txt
+getfacl testfile.txt
