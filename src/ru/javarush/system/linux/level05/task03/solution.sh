7183e402aa18ba0145ad127d34fea6498ba01151
Vetal <heavyve@gmail.com>
2025-10-14 16:15:55 +0300

    add solution javarush linux level05 task 03

--- src/ru/javarush/system/linux/level05/task03/solution.sh
+++ src/ru/javarush/system/linux/level05/task03/solution.sh
@@ -6,14 +6,14 @@ sudo groupadd developers
 # Создаем пользователей dev1 и dev2
 sudo adduser dev1
 sudo adduser dev2
-sudo usermode -a -G developers dev1
+sudo usermode -aG developers dev1
 sudo usermode -a -G developers dev2
 
 # Создаем директорию /shared_folder
-touch /shared_folder
+sudo mkdir -p /shared_folder
 
 # Устанавливаем владельцем группы директории /shared_folder группу developers
-chown :developers /shared_folder
+sudo chown :developers /shared_folder
 
 # Настраиваем права доступа к директории /shared_folder
-chmod 070 shared_folder
+sudo chmod 770 /shared_folder
