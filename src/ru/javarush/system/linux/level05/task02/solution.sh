4afba5ead1ffd3018b2c34f3600b4622b6050230
Vetal <heavyve@gmail.com>
2025-10-14 16:15:50 +0300

    add solution javarush linux level05 task 02

--- src/ru/javarush/system/linux/level05/task02/solution.sh
+++ src/ru/javarush/system/linux/level05/task02/solution.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Создаем пользователя testuser
-sudo adduser testuser
+sudo adduser testuser --disabled-password --gecos ""
 
 # Устанавливаем пароль для пользователя testuser длиной не менее 8 символов
-passwd testuser #12345678
+#passwd testuser #12345678
+echo "testuser:12345678" | sudo chpasswd
 
 # Создаем файл secure_file.txt в домашней директории пользователя testuser
-touch ~/secure_file.txt
+sudo -u testuser touch /home/testuser/secure_file.txt
 
 # Устанавливаем права доступа, чтобы только владелец файла мог читать и записывать его
-chmod 600 ~/secure_file.txt
+sudo chmod 600 /secure_file.txt
