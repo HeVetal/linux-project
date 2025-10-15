974b084bdf32b5ff9f0e96def32a98e496f34bc9
Vetal <heavyve@gmail.com>
2025-10-15 15:34:57 +0300

    add solution javarush linux level05 task 13

--- src/ru/javarush/system/linux/level05/task13/solution.sh
+++ src/ru/javarush/system/linux/level05/task13/solution.sh
@@ -4,7 +4,7 @@
 touch testfile.txt
 
 # Устанавливаем ACL права по умолчанию (необязательно, если файл создан только что)
-?
+setfacl -m u:"$(whoami)":rw testfile.txt
 
 # Выводим текущие ACL права файла testfile.txt
 getfacl testfile.txt
