ba4f20c1c5da13a0c5a8fff95fd80c6315b1f9e8
Vetal <heavyve@gmail.com>
2025-10-22 20:05:12 +0300

    add solution javaRush linux level06 task18

--- src/ru/javarush/system/linux/level06/task18/solution.sh
+++ src/ru/javarush/system/linux/level06/task18/solution.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Создаем архив my_backup.tar из содержимого папки my_folder
-
+sudo tar -cvf my_backup.tar my_folder
 
 # Сжимаем архив с помощью gzip и получаем файл my_backup.tar.gz
+sudo gzip my_backup.tar
+
+#sudo tar -czvf my_backup.tar.gz my_folder
