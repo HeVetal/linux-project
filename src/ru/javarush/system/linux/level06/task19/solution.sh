6c7146ad947cb00be36d237ee44c5a45a5e098bf
Vetal <heavyve@gmail.com>
2025-10-22 20:05:17 +0300

    add solution javaRush linux level06 task19

--- src/ru/javarush/system/linux/level06/task19/solution.sh
+++ src/ru/javarush/system/linux/level06/task19/solution.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Распаковка архива `my_archive.tar.gz` в текущую директорию
-???
+sudo tar - xzvf my_archive.tar.gz
 
 # Отображение содержимого текущей директории после распаковки
-???
+ls my_archive
