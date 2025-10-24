124b627de320629672989765896af25de0db2906
Vetal <heavyve@gmail.com>
2025-10-24 13:02:42 +0300

    add solution javaRush linux level06 task20

--- src/ru/javarush/system/linux/level06/task20/solution.sh
+++ src/ru/javarush/system/linux/level06/task20/solution.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # 1. Создайте zip-архив из папки project_folder
-???
+sudo zip -r project.zip /project_folder
 
 # 2. Извлеките содержимое архива в директорию /tmp/project
-???
-???
+sudo mkdir -p "tmp/project"
+sudo unzip project.zip -d /tmp/project
+
