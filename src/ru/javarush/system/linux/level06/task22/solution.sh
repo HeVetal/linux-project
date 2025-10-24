dd79d24980440247efc762fbf34bcf5ad9f93622
Vetal <heavyve@gmail.com>
2025-10-24 13:02:54 +0300

    add solution javaRush linux level06 task22

--- src/ru/javarush/system/linux/level06/task22/solution.sh
+++ src/ru/javarush/system/linux/level06/task22/solution.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Скопируйте содержимое домашней директории на удалённый сервер с помощью rsync
-rsync -avz -e "ssh -i ~/.ssh/id_rsa" ~ ??? 
+rsync -avz -e "ssh -i ~/.ssh/id_rsa" ~ backup_user@192.168.1.100:/backups/home_backup
