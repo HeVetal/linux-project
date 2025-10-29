d67167adc8cd164d66bec395adde9a739a45d192
vetal <heavyve@gmail.com>
2025-10-29 12:00:20 +0300

    add solution JavaRush tasks Linux level07 task01

--- src/ru/javarush/system/linux/level07/task01/solution.sh
+++ src/ru/javarush/system/linux/level07/task01/solution.sh
@@ -5,7 +5,8 @@ sudo apt update
 sudo apt install -y nginx
 
 # Запустите Nginx и проверьте его статус
-???
+sudo systemctl start nging
+sudo systemctl status nging
 
 # Откройте браузер и проверьте http://localhost
 echo "Перейдите в браузере по адресу http://localhost, чтобы увидеть стандартную страницу Nginx."
