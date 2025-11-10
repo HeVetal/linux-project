245b4740c484b92e3f0f4f9dcf5a649e14c187a1
Vetal <heavyve@gmail.com>
2025-11-10 14:51:18 +0300

    add solution javaRush linux level07 task14

--- src/ru/javarush/system/linux/level07/task14/solution.sh
+++ src/ru/javarush/system/linux/level07/task14/solution.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Для Nginx: получение SSL-сертификата
-???
+sudo certbot --nginx
 
 # Для Apache: получение SSL-сертификата
-???
+sudo certbot --apache
 
 # Убедитесь, что сертификат был успешно установлен
-???
+sudo certbot certificates
 
 # Протестируйте автоматическое обновление сертификатов
 sudo certbot renew --dry-run
