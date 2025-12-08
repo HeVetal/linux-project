d51dc11ce76356e716b83fc68cad7b561da6b8e2
Vetal <heavyve@gmail.com>
2025-12-08 15:53:32 +0300

    add solution javaRush linux level06 task13

--- src/ru/javarush/system/linux/level06/task13/solution.sh
+++ src/ru/javarush/system/linux/level06/task13/solution.sh
@@ -2,12 +2,17 @@
 
 # 1. Посмотрите список устройств
 sudo lsblk
+DEVICE="$(lsblk -nrpo NAME,TYPE | awk '$2=="part" {print $1; exit}')"
+if [[ -z "${DEVICE:-}" ]]; then
+  echo "Не удалось определить съёмный раздел с помощью lsblk." >&2
+  exit 1
+fi
 
 # 2. Создайте точку монтирования
 sudo mkdir -p /mnt/usb
 
 # 3. Смонтируйте устройство
-sudo mount /dev/sbd1 /mnt/usb
+sudo mount "$DEVICE" /mnt/usb
 
 # 4. Проверьте содержимое точки монтирования
 ls /mnt/usb
