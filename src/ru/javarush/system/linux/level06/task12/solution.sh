3cc996dbaa9b95bc0ffcad4112c2eb171c0e69b1
Vetal <heavyve@gmail.com>
2025-12-08 15:53:27 +0300

    add solution javaRush linux level06 task12

--- src/ru/javarush/system/linux/level06/task12/solution.sh
+++ src/ru/javarush/system/linux/level06/task12/solution.sh
@@ -7,8 +7,9 @@ dd if=/dev/zero of=disk.img bs=1M count=100
 sudo mkfs.ext4 ./disk.img
 
 # 3. Монтирование диска
-sudo mount ./disk.img /mnt/test/
-???
+mkdir /mnt/test/
+sudo mount -o loop ./disk.img /mnt/test/
+
 
 # 4. Создание файла и симуляция повреждения данных
 touch /mnt/test/testfile
@@ -18,4 +19,4 @@ sudo dd if=/dev/urandom of=/mnt/test/testfile bs=1M count=1 conv=notrunc
 sudo umount /mnt/test/
 
 # 6. Проверка и исправление файловой системы
-fsck ./disk/img
+fsck -y ./disk/img
