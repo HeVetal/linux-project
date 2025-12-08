681cb31ab335e9d1040432d69a4c7f736920ea57
Vetal <heavyve@gmail.com>
2025-12-08 15:53:37 +0300

    add solution javaRush linux level06 task14

--- src/ru/javarush/system/linux/level06/task14/solution.sh
+++ src/ru/javarush/system/linux/level06/task14/solution.sh
@@ -4,11 +4,12 @@
 sudo lsblk
 
 # 2. Определите тип файловой системы устройства
-
+sudo blkid /dev/sbd
 FILE_SYSTEM=$(blkid -o value -s TYPE /dev/sdb1)
 
 # 3. Смонтируйте устройство с указанием типа файловой системы
-sudo mount -t $FILE_SYSTEM /dev/sdb1 /mnt/usb
+sudo mkdir -p /mnt/usb
+sudo mount -t "$FILE_SYSTEM" /dev/sdb1 /mnt/usb
 
 
 # 4. Проверьте содержимое точки монтирования
